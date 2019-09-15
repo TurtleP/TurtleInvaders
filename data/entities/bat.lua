3f9f61039c613e73f7d55043f6241be8a2245a25
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2019-09-15 12:35:38 -0400

    clean up bat anim, add health icon

--- data/entities/bat.lua
+++ data/entities/bat.lua
@@ -5,20 +5,12 @@ for i = 1, 3 do
     quads[i] = love.graphics.newQuad((i - 1) * 60, 0, 60, 28, sprite:getWidth(), sprite:getHeight())
 end
 
-local animation =
-{
-    idle =
-    {
-        rate = 8, frames = {1, 2, 3}
-    }
-}
-
 return function(entity, x, y)
     entity.name = "bat"
 
     return entity
     :give(Components.Collision)
-    :give(Components.Animation, animation)
+    :give(Components.Animation)
     :give(Components.Sprite, sprite, quads, nil)
     :give(Components.Mask, function(player, item)
         if item.isPassive then
